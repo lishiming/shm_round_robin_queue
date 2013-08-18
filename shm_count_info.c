177d48a2d360caf94dd56d488e4ca61150fcdecb
lishiming <lishiming2007@gmail.com>
2013-08-18 21:35:21 +0800

    Create shm_count_info.c

---
+++ shm_count_info.c
@@ -0,0 +1,32 @@
+#include "shm_count_info.h"
+#include "shm_core.h"
+
+
+unsigned long long  get_op_total(void*handle)
+{	
+	if(NULL == handle)
+	{
+		return SHM_FAIL;
+	}
+	
+	shm_queue_handle_t *shmq_handle = (shm_queue_handle_t*)handle;
+	
+	return shmq_handle->op_total;
+}
+
+
+
+int  get_miss_stat(void*handle, int id, unsigned long long *miss_block, unsigned long long *miss_betys)
+{
+	shm_queue_handle_t *shmq_handle = (shm_queue_handle_t*)handle;
+	
+	if(NULL == shmq_handle)
+	{
+		return SHM_FAIL;
+	}
+	int i = get_read_num(id);
+
+	*miss_block = shmq_handle->readers_miss[i].miss_block_num;
+	*miss_betys = shmq_handle->readers_miss [i].miss_bytes;
+	return SHM_OK;
+}
