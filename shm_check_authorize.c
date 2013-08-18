5f8fba202597d7809b84b031741762aea3e1b761
lishiming <lishiming2007@gmail.com>
2013-08-18 23:03:28 +0800

    Update shm_check_authorize.c

--- shm_check_authorize.c
+++ shm_check_authorize.c
@@ -1,19 +1,5 @@
 #include "shm_check_authorize.h"
-/****************************************************************
-	this file is used for checking the process's authorize
-	if the process don't have a right id, the interface
-	will refuse the read or write operation.
- ***************************************************************/
 
-
-
-/**
- * 
- * METHOD: check_read_id
- * DESCRIPTION: check the id whether has been authorized  
- * @param :id, the read id who is distributed to the read process,not pid!
- * @return :SHM_TRUE if this id has been authorized,if not,SHM_FALSE is returned
- */
 
 int check_read_id(int id)
 {
@@ -30,13 +16,6 @@ int check_read_id(int id)
 }
 
 
-/**
- * METHOD: get_read_num
- * DESCRIPTION: actually，the id can be mapped to a num between 1-32,we can use this num to stand for the process
- * @param :id,the read id who is distributed to the read process,not pid!
- * @return :the num between 1-32
- */
- 
 int get_read_num(int id)
 {
 	int num = ((id & 0xffff0000) >>(32 - 16)) | ((id & 0xffff0000) << 16);
@@ -44,15 +23,6 @@ int get_read_num(int id)
 }
 
 
-
-/**
- * 
- * METHOD: check_write_id
- * DESCRIPTION: check the id whether has been authorized  
- * @param :id, the write id who is distributed to the write process,not pid!
- * @return :SHM_TRUE if this id has been authorized,if not,SHM_FALSE is returned
- */
- 
 int check_write_id(int id)
 {
 	int m= id & 0x0000ffff ^ 0x0000814a ^ 0x0000f4e0;
@@ -66,13 +36,7 @@ int check_write_id(int id)
 	}
 }
 
-/**
- * METHOD: get_write_num
- * DESCRIPTION: actually，the id can be mapped to a num between 1-32,we can use this num to stand for the process
- * @param :id,the write id who is distributed to the write process,not pid!
- * @return :the num between 1-32
- */
- 
+
 int get_write_num(int id)
 {
 	int num = ((id & 0xffff0000) >>(32 - 16)) | ((id & 0xffff0000) << 16);
