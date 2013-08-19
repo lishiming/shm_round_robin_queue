284ef33c1b7500624a3e4c6f6163827b6b39ec4f
lishiming <lishiming2007@gmail.com>
2013-08-19 18:08:30 +0800

    Update shm_count_info.h

--- shm_count_info.h
+++ shm_count_info.h
@@ -4,10 +4,28 @@
 
 extern int check_read_id(int id);
 
-
+/**
+ * 
+ * METHOD: get_op_total
+ * DESCRIPTION: get the  total data process has read or writen  
+ * @param : handle,the handle created
+ * @return : how many bytes process has read or writen
+ */
+ 
 unsigned long long  get_op_total(void*handle);
 
 
-int get_miss_stat(void*handle, int id, unsigned long long *miss_block, unsigned long long *miss_betys);
+/**
+ * 
+ * METHOD: get_miss_stat
+ * DESCRIPTION: this function is used by write processto get a read process miss info 
+ * @param : handle,the handle created
+ * @param : id, read process's id(not pid)
+ * @param : miss_block, output how many blocks the reader has missed
+ * @param : miss_bytes, output how many bytes the reader has missed
+ * @return : SHM_OK if success,SHM_FAIL if fail
+ */
+ 
+int get_miss_stat(void*handle, int id, unsigned long long *miss_block, unsigned long long *miss_bytes);
 
 #endif
