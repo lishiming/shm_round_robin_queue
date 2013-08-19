2b193fda5147b06be37f01d8ea4b5637aaffc91b
lishiming <lishiming2007@gmail.com>
2013-08-19 22:11:35 +0800

    Update shm_inner_func.h

--- shm_inner_func.h
+++ shm_inner_func.h
@@ -82,26 +82,58 @@ int  copy_info_from_handle(char *sh_mem, shm_queue_handle_t *shmq_handle);
  * @return :SHM_TRUE if success,if not,SHM_FALSE is returned
  */
  
-int  init_shm_info(char *sh_mem, shm_queue_handle_t *shmq_handle);
+int init_shm_info(char *sh_mem, shm_queue_handle_t *shmq_handle);
 
 
 /**
  * 
  * METHOD: read_block
  * DESCRIPTION: read a block of data from shared memory to process's buf
- * @param :sh_mem, the address of shared memory
- * @param :shmq_handle,
- * @return :SHM_TRUE if success,if not,SHM_FALSE is returned
+ * @param : to_read_block,block info of data to be read
+ * @param : shmq_handle,handle
+ * @param : to_read_buf,the buf to store data
+ * @param : buf_len, length of buf
+ * @return : SHM_TRUE if success,if not,SHM_FALSE is returned
  */
+ 
 int read_block(block_info_t *to_read_block, shm_queue_handle_t *shmq_handle, char *to_read_buf, int buf_len);
 
 
+/**
+ * 
+ * METHOD: create_new_block
+ * DESCRIPTION: create a new block info to the queue
+ * @param :shmq_handle,
+ * @param : buf_len,length of data
+ * @param : is_split,SHM_NO_SPLITS or SHM_SPLITS
+ * @return :SHM_TRUE if success,if not,SHM_FALSE is returned
+ */
+ 
 int create_new_block(shm_queue_handle_t *shmq_handle, int buf_len, int is_split);
 
 
+/**
+ * 
+ * METHOD: update_handle_info
+ * DESCRIPTION: update handle's info after read or write
+ * @param :shmq_handle,
+ * @param : buf_len,length of data
+ * @return :SHM_OK if success,if not,SHM_FAIL is returned
+ */
+ 
 int update_handle_info(shm_queue_handle_t *shmq_handle, int buf_len);
 
 
+/**
+ * 
+ * METHOD: write_block
+ * DESCRIPTION: write a block of data from shared memory from process's buf
+ * @param : shmq_handle,handle
+ * @param : to_write_buf ,pointer of buf
+ * @param : buf_len, length of buf
+ * @return :SHM_OK if success,if not,SHM_FAIL is returned
+ */
+ 
 int write_block(shm_queue_handle_t *shmq_handle,const char *to_write_buf,int buf_len);
 
 
