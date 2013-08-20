77766f5427f4b5db3033e6e576bc1d2597f3d6fa
lishiming <lishiming2007@gmail.com>
2013-08-20 14:45:40 +0800

    Update shm_core.h

--- shm_core.h
+++ shm_core.h
@@ -48,9 +48,9 @@ enum SHM_RETURN_RET{
 			SHM_NO_AUTHORITY, /* error:the process don't have been authorized*/
 			SHM_UNKNOWN_TYPE, /* error:set a unknown type*/
 			SHM_NO_SHM, /* error:the share memory is not exist*/
-			SHM_CREATE_SHMEM_FAIL, /* error: fail to create share memory*/
-			SHM_ALREADY_EXIST, /* error: the share memory is exist,can't create it repeatedly*/
-			SHM_NO_DATA, /* error: no data in the share memory*/
+			SHM_CREATE_SHMEM_FAIL, /* error: fail to create shared memory*/
+			SHM_ALREADY_EXIST, /* error: the shared memory is exist,can't create it repeatedly*/
+			SHM_NO_DATA, /* error: no data in the shared memory*/
 			SHM_DATA_READED_YET, /* error: this data block has been read by the same process*/
 			SHM_OUT_QUEUE_FAIL, /* error: fail to dequeue the tail*/
 			SHM_NO_SPACE, /* error: no space left to write data to share memory*/
@@ -63,7 +63,7 @@ enum SHM_RETURN_RET{
 /**
  * 
  * STRUCT NAME: shm_queue_info_t
- * DESCRIPTION: this struct contains the basic info of share memory  
+ * DESCRIPTION: this struct contains the basic info of shared memory  
  */
  
 typedef struct _shm_queue_info
@@ -71,12 +71,12 @@ typedef struct _shm_queue_info
 	unsigned int max_queue_size; /* the max elements's num of the queue*/
 	unsigned int pool_size; /* size of the memory pool(data block will be here)*/
 	unsigned int pool_left_size; /* size of the memory pool can be used*/
-    	unsigned int pool_begin_offset; /* the offset of the memory pool's start to the start of the whole share memory*/
-    	unsigned int pool_end_offset; /*the offset of the memory pool's end to the start of the whole share memory*/
-    	unsigned int write_pos; /* the offset of the next write position to the start of the whole share memory*/
-	unsigned int owner_id; /* which process create the share memory(not pid)*/
-	unsigned int op_num; /* the num of processes who attached to the share memory*/
-	char read_flags[MAX_PROC_NUM]; /* this array marks which reader is reading share memory*/
+    	unsigned int pool_begin_offset; /* the offset of the memory pool's start to the start of the whole shared memory*/
+    	unsigned int pool_end_offset; /*the offset of the memory pool's end to the start of the whole shared memory*/
+    	unsigned int write_pos; /* the offset of the next write position to the start of the whole shared memory*/
+	unsigned int owner_id; /* which process create the shared memory(not pid)*/
+	unsigned int op_num; /* the num of processes who attached to the shared memory*/
+	char read_flags[MAX_PROC_NUM]; /* this array marks which reader is reading shared memory*/
 	pthread_mutex_t mutex; /* mutext protect op_num*/
     	pthread_rwlock_t rwlock; /* writer should get rwlock when write,reader only get trwlock when he first get head of queue*/
        
@@ -115,8 +115,8 @@ typedef struct _shm_queue_handle_t
 	int	shm_fd; /* mid of share memory*/
 	unsigned int	para_queue_size; /* the queue elements' num*/
 	unsigned int	para_pool_size; /* size of pool to store data*/
-	miss_reader_stat_t	readers_miss[MAX_PROC_NUM]; /* if WRITE_PRIO,how many betys the readers have missed*/	
-	shm_queue_info_t	*shm_que_info; /* start of share memry*/
+	miss_reader_stat_t	readers_miss[MAX_PROC_NUM]; /* if WRITE_PRIO,how many bytes the readers have missed*/	
+	shm_queue_info_t	*shm_que_info; /* start of shared memry*/
 	circular_array_t	*que; /* start of array*/
 
 }shm_queue_handle_t;
